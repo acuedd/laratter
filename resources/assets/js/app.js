e7812e733ab0f1cf7dc25f3200b232947aced058
Edward Acu <acued89@gmail.com>
2017-10-24 13:25:59 -0600

    Obtención de respuestas en el frontend de la aplicación
    Utilización de vue js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example', require('./components/Example.vue'));
+Vue.component('responses', require('./components/Responses.vue'));
 
 const app = new Vue({
     el: '#app'
